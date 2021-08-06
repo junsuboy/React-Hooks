e8b9bac5c2ed0381717208469b24113bab7c08d5
junsuboy <cobijraynor@naver.com>
2021-08-06 13:12:52 +0900

    Create useNotification.js

---
+++ react-hooks/useNotification/useNotification.js
@@ -0,0 +1,22 @@
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission == "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+
+  // you can search 'options' here
+  // https://developer.mozilla.org/ko/docs/Web/API/notification
+};
